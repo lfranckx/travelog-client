40617247da138bc7cf8eb65899d2165e338215af
Bowie <bowie@thinkful.com>
2020-05-18 10:46:54 -0700

    favicon

--- src/config.js
+++ src/config.js
@@ -1,5 +1,4 @@
 export default {
     API_ENDPOINT: "https://still-lake-35765.herokuapp.com/api",
-    // API_ENDPOINT: "http://localhost:8000/api",
     TOKEN_KEY: '6%7AcgLG"2oBiR76%7AcgLG"2oBiR7DTj43Dwq8b'
-}
+};
